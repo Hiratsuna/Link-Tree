bc9345c6e76c11720a84563c5218da391c5f8490
Hiratsuna <adhdmicromanagement@outlook.com>
2023-08-14 01:33:20 +0300

    links~

---
+++ links.js
@@ -0,0 +1,58 @@
+export const links = [
+	{
+		name: "Twitter",
+		link:
+"https://twitter.com/casualnerdingtv",
+		image:
+"twitter.png",
+	},
+	{
+		name: "Linkedin",
+		link:
+"https://www.linkedin.com/in/gerganazhekova/",
+		image:
+"linkedin-sign.png"
+	},
+	{
+		name: "Instagram",
+		link:
+"https://www.instagram.com/bimit0oo",
+		image:
+"instagram (1).png",
+	},
+	{
+		name: "PayPal",
+		link:
+"https://paypal.me/casualnerd?country.x=BG&locale.x=en_US",
+		image:
+"love.png",
+	},
+	{
+		name: "Github",
+		link:
+"https://github.com/Hiratsuna",
+		image:
+"github-sign.png",
+	},
+	{
+		name: "YouTube",
+		link:
+"https://www.youtube.com/@casualnerdingyt",
+		image:
+"youtube.png",
+	},
+	{
+		name: "Discord",
+		link:
+"https://discord.gg/9FGPYQFXpd",
+		image:
+"discord.png",
+	},
+	{
+		name: "Meow",
+		link:
+"https://casualnerding.online",
+		image:
+"4.png",
+	},
+];
